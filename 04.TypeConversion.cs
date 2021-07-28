f8fd190c9cc77373b2c2c83911ef07b3d7e733c5
CainLuo <350116542@qq.com>
2021-07-28 18:32:14 +0800

    Create 04.TypeConversion.cs

---
+++ 04.TypeConversion.cs
@@ -0,0 +1,52 @@
+using System;
+namespace TypeConversionApplication
+{
+    class ExplicitConversion
+    {
+        static void Main(string[] args)
+        {
+            double d = 5673.74;
+            int i;
+
+            // 强制转换double为int
+            i = (int)d;
+            Console.WriteLine(i);
+            Console.ReadKey();
+        }
+    }
+
+    class StringConversion
+    {
+        static void Main(string[] args)
+        {
+            int i = 75;
+            float f = 53.005f;
+            double d = 2345.7652;
+            bool b = true;
+
+            Console.WriteLine(i.ToString());
+            Console.WriteLine(f.ToString());
+            Console.WriteLine(d.ToString());
+            Console.WriteLine(b.ToString());
+            Console.WriteLine("\n");
+            Console.ReadKey();
+
+            StringConversion.StringToInt();
+        }
+
+        static void StringToInt()
+        {
+            string locstr = 123.ToString();
+
+            // 方法一：用Convert
+            int i = Convert.ToInt16(locstr);
+
+            // 方法二：用Parse
+            int ii = int.Parse(locstr);
+
+            Console.WriteLine(i);
+            Console.WriteLine(ii);
+            Console.ReadKey();
+        }
+    }
+}
