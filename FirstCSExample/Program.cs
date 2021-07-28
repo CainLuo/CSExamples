aa24c7645af48dbe0448d07e05fc648de138cd84
CainLuo <350116542@qq.com>
2021-07-29 00:07:27 +0800

    Update projects

---
+++ FirstCSExample/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace FirstCSExample {
+    class MainClass {
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
